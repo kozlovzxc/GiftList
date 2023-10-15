2b404a5959140b42659b6e945132cf2ab1d71c5b
Nikita <kozlovzxc@gmail.com>
2023-10-15 13:26:12 +0200

    simple airdrop

--- client/index.js
+++ client/index.js
@@ -4,14 +4,20 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
-async function main() {
+async function main(name) {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  const tree = new MerkleTree(niceList)
+  const leafIndex = niceList.findIndex(leaf => leaf === name)
+  const proof = tree.getProof(leafIndex)
+
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    data: name,
+    proof: proof,
   });
 
   console.log({ gift });
 }
 
-main();
+main(niceList[Math.trunc(Math.random() * niceList.length)]);
+main('qweqwe')
